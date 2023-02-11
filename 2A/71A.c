f73f99736b047874f5c2ec7ca772d0cc4e703736
shaluKm <shalu.dakshana16@gmail.com>
2023-02-11 17:29:39 +0530

    word too long

---
+++ 2A/71A.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+	int n,i,j,k,l;
+	char word[100];
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+
+          //scanf("%[^\n]%*c", word);
+
+          scanf("%s", word);
+          l=strlen(word);
+          if(l>10)
+          {
+          	printf("%c%d%c\n",word[0],(l-2),word[(l-1)] );
+          }
+          else
+          {
+          	printf("%s\n",word );
+          
+          }
+          
+	}
+	
+	
+	return 0;
+}
